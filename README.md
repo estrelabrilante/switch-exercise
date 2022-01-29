4ad7d67d8e51d03bcecf78fd7549e887a0df8552
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-01-29 14:44:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Switch Exercise
+
+# Instructions
+
+Create a program that takes a number (`Int`) as an input and prints the corresponding day of the week using a `switch` statement.
+
+e.g. If the input was `5`
+
+Then the function should print:
+
+"Friday"
+
+If the input number does not match a day of the week then your program should print "Error".
+
+The weekday printed needs to have the first letter capitalised and spelt correctly.
+
+
+# Solution
+
+[Link to Solution Code](https://gist.github.com/TheMuellenator/ceed75150b5f7127c8bf38b3564e7ca3)
+
---
+++ main.swift
@@ -0,0 +1,33 @@
+////Don't change this
+var aNumber =  Int(readLine()!)! 
+
+func dayOfTheWeek(day: Int) {
+  
+  //Write your code inside this function.
+  switch day{
+      case 1:
+      print("Monday")
+      case 2:
+      print("Tuesday")
+      case 3:
+      print("Wednesday")
+      case 4:
+      print("Thursday")
+      case 5:
+      print("Friday")
+      case 6:
+      print("Saturday")
+      case 7:
+      print("Sunday")
+      default:
+      print("Error")
+      }
+  
+  
+  
+  
+}
+
+//Try out some different numbers here
+//Don't copy the line below into Udemy
+dayOfTheWeek(day: 5)
